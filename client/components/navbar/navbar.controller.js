0501c20e741c362fef4d457b10d8c1dc4f5f3a58
Patrick Michelberger <patrick.michelberger@tum.de>
2016-05-27 13:59:17 +0200

    add sidebar

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,19 +1,55 @@
 'use strict';
 
 class NavbarController {
-  //start-non-standard
-  menu = [{
-    'title': 'Home',
-    'state': 'main'
-  }];
-
   isCollapsed = true;
-  //end-non-standard
 
-  constructor(Auth) {
+  constructor(Auth, $timeout, $log, $mdSidenav) {
+    this.$timeout = $timeout;
+    this.$log = $log;
+    this.$mdSidenav = $mdSidenav;
+
     this.isLoggedIn = Auth.isLoggedIn;
     this.isAdmin = Auth.isAdmin;
     this.getCurrentUser = Auth.getCurrentUser;
+    this.toggleLeft = this.buildDelayedToggler('left');
+  }
+
+  /**
+   * Close sidebar
+   */
+  close() {
+    var self = this;
+    this.$mdSidenav('left').close()
+    .then(function () {});
+  }
+
+  /**
+    * Supplies a function that will continue to operate until the
+    * time is up.
+    */
+   debounce(func, wait, context) {
+     var timer;
+     return function debounced() {
+       var self = this,
+           args = Array.prototype.slice.call(arguments);
+       self.$timeout.cancel(timer);
+       timer = self.$timeout(function() {
+         timer = undefined;
+         func.apply(context, args);
+       }, wait || 10);
+     };
+   }
+
+  /**
+   * Build handler to open/close a SideNav
+   */
+  buildDelayedToggler(navID) {
+    var self = this;
+    return self.debounce(function() {
+      self.$mdSidenav(navID)
+        .toggle()
+        .then(function () {});
+    }, 200);
   }
 }
 
