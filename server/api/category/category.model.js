4cfefefb9b166401b03824676cdac25d99e79653
Admir Jashari <admir-j@hotmail.com>
2016-06-28 17:41:16 +0200

    Get food subcategories from Walmart API

--- server/api/category/category.model.js
+++ server/api/category/category.model.js
@@ -2,14 +2,24 @@
 
 var mongoose = require('mongoose');
 
+var subChildren = {
+  id: String,
+  name: String,
+  path: String
+};
+
+var children = {
+  id: String,
+  name: String,
+  path: String,
+  children: [ subChildren ]
+};
+
 var CategorySchema = new mongoose.Schema({
   id: String,
   name: String,
-  categoryPath: String,
-  childrens: [{
-    id: String,
-    name: String
-  }]
+  path: String,
+  children: [ children ]
 }, {
   collection: 'seba-categories'
 });
